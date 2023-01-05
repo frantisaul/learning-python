bd4c33c45bad64154fea7d5f89bf08d4f7d53047
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-05 18:12:51 -0500

    added transformations

---
+++ platzi/07_transformation.py
@@ -0,0 +1,20 @@
+name = 'Franti'
+print(name)
+print(type(name))
+
+name = 10
+print(name)
+print(type(name))
+
+print('Franti' + 'Huaman')
+print(10+20)
+
+age = 12
+print('Mi edad es', str(12))
+print(f'Mi edad es {age}')
+
+age = input('Escribe tu edad: ')
+print(type(age))
+age = int(age)
+age += 10
+print(f'Tu edad en 10 años será {age}')
