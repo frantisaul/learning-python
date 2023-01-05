4b226825ff67be722cc122c97b26a7ffbadde0a9
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-05 00:06:22 -0500

    Added `types`

---
+++ platzi/03_types.py
@@ -0,0 +1,17 @@
+# strings
+my_name = 'Franti'
+my_name = 'Saul'
+print(my_name)
+
+# printing the kind of data
+print(type(my_name))
+
+# int
+my_age = 12
+print('my age => ', my_age)
+print(type(my_age))
+
+# boolean
+is_single = True
+print('is single => ', is_single)
+print(type(is_single))
