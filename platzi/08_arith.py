bd7ddf81b7c919f6c058bb742d4ee21791d66ba2
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-05 18:37:39 -0500

    added arith

---
+++ platzi/08_arith.py
@@ -0,0 +1,25 @@
+print(10+10)
+print(10-5)
+print(10*2)
+print(10/2)
+
+print('Hola' + 'mundo')
+print('Hola' * 3)
+
+print(10/3)
+print(10%3)
+print(10//3) # only save integer number
+print(2 ** 3)
+
+# P.E.M.D.A.S
+# P: Paréntesis
+# E: Exponentes
+# M: Multiplicación.
+# D: División.
+# A: Adición.
+# S: Sustracción.
+print('\nPEMDAS')
+print(2 ** 3 + 3 - 7 / 1 // 4)
+print(2**3)
+print((7 / 1) // 4)
+print(8+3-1)
