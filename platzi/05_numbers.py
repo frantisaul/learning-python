eb7ed6cf4d3fef634bd30492cf4bd14cfc1ffa4c
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-05 16:27:45 -0500

    added numbers

---
+++ platzi/05_numbers.py
@@ -0,0 +1,24 @@
+lives = 3
+print(type(lives))
+
+age = 12
+budget = 100
+temperatue = 12.12
+print(type(temperatue))
+
+lives += 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+number = 450000000000000000.1
+print(number)
+
+# challenge
+january = 200
+febrary = 520
+march = 230
+
+average = january + febrary + march / 3
+print(average)
