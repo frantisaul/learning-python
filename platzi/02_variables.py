ba79670a560a9fced4066b7ddf3a83a7b31fbf06
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-04 23:58:39 -0500

    Added `02_variables.py`

---
+++ platzi/02_variables.py
@@ -0,0 +1,17 @@
+
+# We don't need to declare the type.
+my_name = 'Franti'
+print(my_name)
+
+my_age = 12
+print(my_age)
+
+my_name = 'Saul'
+print(my_name)
+
+# input
+my_name = input("What's your name? ")
+print('reading input: ', my_name)
+
+my_age = input("How old are you? ")
+print('reading input: ',my_age)
