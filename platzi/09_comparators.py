9c8cd747f278a7cebe34932d6cf57d3e2aae994b
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-06 05:23:25 -0500

    added comparators

---
+++ platzi/09_comparators.py
@@ -0,0 +1,44 @@
+# >
+print('\n>')
+print(7>3)
+print(3>7)
+print(7>7)
+
+# <
+print('\n<')
+print(5<6)
+print(6<5)
+print(5<5)
+
+# >=
+print('\n>=')
+print(2>=1)
+print(2>=3)
+print(2>=2)
+
+
+# <=
+print('\n<=')
+print(1<=2)
+print(2<=1)
+print(2<=2)
+
+# ==
+print('\n==')
+print(6==6)
+print(5==2)
+
+# !=
+print('\n!=')
+print(6!=10)
+print(6!=6)
+
+# comparing strings
+print('\nComparing strings')
+print('Apple' == 'Apple')
+print('Apple' == 'apple')
+print('1' == 1)
+
+print('\n')
+age = 18
+print(age >= 18)
