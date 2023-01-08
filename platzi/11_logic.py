c7790ecfcf25a57617fc5c9fea7bc23b8ac851ef
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-07 23:03:09 -0500

    added logic

---
+++ platzi/11_logic.py
@@ -0,0 +1,19 @@
+# and
+print('AND')
+print('True and True => ', True and True)
+print('True and False => ', True and False)
+print('False and True =>', False and True)
+print('False and False => ', False and False)
+
+print(10 > 5 and 5<10)
+print(10 > 5 and 5>10)
+
+# example
+print('\n')
+stock = input('Ingrese el numero de stock => ')
+stock = int(stock)
+print(stock >= 100 and stock <= 1000)
+
+# or
+role = input('Digita el rol => ')
+print(role == 'admin' or role == 'seller')
