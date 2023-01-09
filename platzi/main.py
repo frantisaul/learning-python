6cdc8d6dec4b351bf6642ee834b66cbf57e9284a
Franti Saúl Huamán Mera <frantisaul@gmail.com>
2023-01-08 20:34:15 -0500

    added `main.py`

---
+++ platzi/main.py
@@ -0,0 +1,26 @@
+user_option = input('Piedra, papel o tijera => ')
+computer_option = 'piedra'
+
+if user_option == computer_option:
+    print('Empate!')
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('piedra ganar a tijera')
+        print('user ganó!')
+    else:
+        print('Papel gana a piedra')
+        print('computer ganó!')
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print('papel ganar a tijera')
+        print('user ganó!')
+    else:
+        print('Tijera gana a papel')
+        print('computer ganó!')
+elif user_option == 'tijera':
+    if computer_option == 'papel':
+        print('tijera ganar a papel')
+        print('user ganó!')
+    else:
+        print('piedra gana a tijera')
+        print('computer ganó!')
